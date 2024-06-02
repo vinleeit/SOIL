42380bdf4041123dfb5a7b510e1b0d5ce34e3c0b
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-06-02 18:10:16 +1000

    feat: add graphql client sample to admin fe

--- admin-fe/src/App.tsx
+++ admin-fe/src/App.tsx
@@ -1,9 +1,30 @@
-function App() {
+import { useQuery, gql } from "@apollo/client";
+
+const FILMS_QUERY = gql`
+  {
+    users {
+      id
+      username
+      isBlocked
+      email
+    }
+  }
+`;
+
+export default function App() {
+  const { data, loading, error } = useQuery(FILMS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+
   return (
-    <>
-      <div className="bg-blue-500 text-white">Admin</div>
-    </>
+    <div>
+      <h1>Admin</h1>
+      <ul>
+        {data.users.map((launch: { id: number; username: string }) => (
+          <li key={launch.id}>{launch.username}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
-
-export default App;
