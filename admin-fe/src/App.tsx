704242808cc448207c5ec6fa091a348aec41566f
Ravel Tanjaya <s3937118@student.rmit.edu.au>
2024-06-03 20:43:44 +1000

    feat: add design for dashboard

--- admin-fe/src/App.tsx
+++ admin-fe/src/App.tsx
@@ -1,31 +1,53 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex bg-black text-white px-8 py-3 items-center">
-        <Link to={"/"}>Admin Panel</Link>
-        <div className="flex-1 flex justify-end space-x-1">
-          <Link
-            to={"/users"}
-            className="bg-white rounded px-3 py-1 text-black hover:bg-blue-200"
+      <nav className="bg-gray-50 border-b-2 border-black">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="#"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            Users
-          </Link>
-          <Link
-            to={"/reviews"}
-            className="bg-white rounded px-3 py-1 text-black hover:bg-blue-200"
+            <span className="self-center text-2xl font-semibold whitespace-nowrap ">
+              <Link to={"/"}>
+                SOIL<sup className="text-sm">Admin</sup>
+              </Link>
+            </span>
+          </a>
+          <div
+            className="hidden w-full md:block md:w-auto"
+            id="navbar-solid-bg"
           >
-            Review
-          </Link>
-          <Link
-            to={"/products"}
-            className="bg-white rounded px-3 py-1 text-black hover:bg-blue-200"
-          >
-            Product
-          </Link>
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
+              <li>
+                <NavLink
+                  to={"/users"}
+                  className="block py-2 px-3 md:p-0 text-gray-500 rounded md:bg-transparent"
+                >
+                  User Moderation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/reviews"}
+                  className="block py-2 px-3 md:p-0 text-gray-500 rounded md:bg-transparent"
+                >
+                  Review Moderation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={"/products"}
+                  className="block py-2 px-3 md:p-0 text-gray-500 rounded md:bg-transparent"
+                >
+                  Product Management
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
       <div className="flex h-full grow">
         <Outlet />
       </div>
